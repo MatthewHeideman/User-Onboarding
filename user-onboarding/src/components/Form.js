663255e6a2e0b2006a9305cc42d6c34a969ba1ac
matthew-heideman <heidemanmr@yahoo.com>
2020-03-26 16:37:23 -0500

    really done, for real this time

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -9,8 +9,8 @@ const formSchema = yup.object().shape({
       .string()
       .email("Must be a valid email address.")
       .required("Must include email address."),
+    password: yup.string().required('Must include password.'),
     terms: yup.boolean().oneOf([true], "please agree to terms of use"),
-    password: yup.string(),
 });
 
 function Form() {
@@ -28,6 +28,7 @@ function Form() {
         terms: "",
     });
     const [post, setPost] = useState([]);
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         formSchema.isValid(formState).then(valid => {
           setButtonDisabled(!valid);
@@ -38,9 +39,11 @@ function Form() {
         axios
           .post("https://reqres.in/api/users", formState)
           .then(res => {
-            setPost(res.data); // get just the form data from the REST api
-            console.log("success", post);
-            // reset form if successful
+            setPost(res.data);
+            setUsers([
+                ...users,
+                res.data
+            ])
             setFormState({
               name: "",
               email: "",
@@ -51,7 +54,6 @@ function Form() {
           .catch(err => console.log(err.response));
       };
     const validateChange = e => {
-        // Reach will allow us to "reach" into the schema and test only one part.
         yup
           .reach(formSchema, e.target.name)
           .validate(e.target.name === "terms" ? e.target.checked : e.target.value)
@@ -67,7 +69,7 @@ function Form() {
               ...errors,
               [e.target.name]: err.errors[0]
             });
-          });
+        });
     };
     const inputChange = e => {
         e.persist();
@@ -82,27 +84,39 @@ function Form() {
       };
     return(
         <div>
-            <form class='form' onSubmit = {formSubmit}>
+            <form className='form' onSubmit = {formSubmit}>
                 <label htmlFor='name'> 
                     Name:
                     <input type='text' name='name' id='name' value={formState.name} onChange={inputChange}/>
+                    {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
                 </label>
                 <label htmlFor='email'>
                     E-Mail:
                     <input type = 'email' name='email' id='email' value={formState.email} onChange={inputChange}/>
+                    {errors.email.length > 0 ? <p className="error">{errors.email}</p> : null}
                 </label>
                 <label htmlFor='password'>
                     Password:
                     <input type = 'password' name='password' id='password' value={formState.password} onChange={inputChange}/>
+                    {errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
                 </label>
                 <label htmlFor='terms'>
                     Terms of Service:
                     <input type='checkbox' name='terms' id='terms' checked={formState.terms} onChange={inputChange}/>
                 </label>
+                <pre>{JSON.stringify(post, null, 2)}</pre>
+                {users.map(item => {
+                    return (
+                        <div>
+                            <p>
+                                {item.name}
+                            </p>
+                        </div>
+                    )
+                })}
                 <button disabled={buttonDisabled}>
                     Submit
                 </button>
-                <pre>{JSON.stringify(post, null, 2)}</pre>
             </form>
         </div>
     )
