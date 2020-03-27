f2ee03f0a606144ceca8fd3ee5073fe859aaa7e4
matthew-heideman <heidemanmr@yahoo.com>
2020-03-26 20:40:12 -0500

    Completed testing form

---
+++ user-onboarding/cypress/integration/examples/Form_test.js
@@ -0,0 +1,23 @@
+describe("Testing our volunteer form", function() {
+    beforeEach(function() {
+      cy.visit("http://localhost:3000/");
+    });
+    it("Add test to inputs and submit form", function() {
+      cy.get('input[name="name"]')
+      .type("Dawson")
+      .should("have.value", "Matthew")
+      cy.get('input[name="email"]')
+      .type("email@email.com")
+      .should("have.value", "email@email.com")
+      cy.get('textarea')
+      .type('I want to help people')
+      .should("have.value", "I want to help people")
+      cy.get('#positions')
+      .select('Yard Work')
+      .should("have.value", "Yard Work")
+      cy.get('[type="checkbox"]')
+      .check()
+      .should("be.checked")
+      cy.get('button').click()
+    });
+  });
